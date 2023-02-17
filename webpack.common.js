c805fb9f6ab4bf5dd37280cf689edeb6113ffe9c
P0m1dOr <sysoletin.alexandr18@gmail.com>
2023-02-17 23:00:03 +0300

    webpack commit

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+const loader = require("sass-loader");
+
+module.exports = {
+    entry: {
+        main: "./src/index.js",
+        vendor: "./src/vendor.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    "html-loader"
+                ]
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outpurPath: "imgs"
+                    }
+                }
+            }
+        ]
+    }
+
+};
