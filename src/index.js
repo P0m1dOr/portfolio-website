09910e5d419993d3f6337e19ff6a31435bbc4415
l1ttlewizard <andrwyandreev2005@gmail.com>
2023-02-20 17:44:18 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import "./main.scss"; 
+
+document.querySelector('.theme-button').addEventListener('click',()=>{
+    var body = document.querySelector('html');
+    console.log('click');
+    if (body.getAttribute('data-bs-theme')=='dark') {
+        body.setAttribute('data-bs-theme', 'light');
+        document.getElementById('btnSwitch').textContent="Switch to Dark Theme";   
+    }
+    else {
+        body.setAttribute('data-bs-theme', 'dark');
+        document.getElementById('btnSwitch').textContent="Switch to Light Theme";
+
+    }
+});
--- src/vendor.js
+++ src/vendor.js
@@ -1,1 +1,5 @@
-import "bootstrap";
+import bootstrap from 'bootstrap';
+import './main.scss'
+import Alert from 'bootstrap/js/dist/alert'
+import { Tooltip, Toast, Popover } from 'bootstrap'
+import _ from 'lodash'
